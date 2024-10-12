f634bf648aaefc2fe804d7d2d9b2a8c3a1a55ecd
Pablo2048 <pablo@xpablo.cz>
2024-10-12 15:20:32 +0200

    GA update

--- update_dev.sh
+++ update_dev.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-git checkout development
+
+# Zkontroluj, jestli existuje větev development na vzdáleném repozitáři
+if git show-ref --quiet refs/heads/development; then
+  git checkout development
+else
+  git checkout -b development origin/development
+fi
+
 git pull origin development
+
+# Proveď merge z main
 git merge main
+
+# Pushni změny do vzdálené development větve
 git push origin development
